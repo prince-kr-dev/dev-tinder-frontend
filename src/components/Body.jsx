5a55d4efd05ee16681debbb3d2b497bfa90da2e0
prince-kr-dev <princekumark2004@gmail.com>
2025-08-27 12:25:02 +0530

    can't access anything without login

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
-import Nav from './Nav';
-import { Outlet } from 'react-router-dom';
-import Footer from './Footer';
+import React, { useEffect } from "react";
+import Nav from "./Nav";
+import { Outlet, useNavigate } from "react-router-dom";
+import Footer from "./Footer";
+import axios from "axios";
+import { URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 function Body() {
-  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!userData) {
+      fetchUser();
+    }
+  }, []);
+
   return (
-    <> 
-        <Nav/>
-        <Outlet/>
-        <Footer/>
+    <>
+      <Nav />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
 
 export default Body;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Nav() {
   const user = useSelector((store) => store.user);
@@ -8,13 +9,13 @@ function Nav() {
     <>
       <div className="navbar bg-base-300 shadow-sm px-2 md:px-10">
         <div className="flex-1">
-          <a href="#" className="btn btn-ghost text-xl">
+          <Link to="/" className="btn btn-ghost text-xl">
             Dev Tinder
-          </a>
+          </Link>
         </div>
         {user && (
           <div className="flex gap-2 items-center">
-            <p className="text-md font-medium">Welcome, {user.user.firstName}</p>
+            <p className="text-md font-medium">Welcome, {user.firstName}</p>
             <div className="dropdown dropdown-end">
               <div
                 tabIndex={0}
@@ -24,7 +25,7 @@ function Nav() {
                 <div className="w-10 rounded-full">
                   <img
                     alt="Tailwind CSS Navbar component"
-                    src={user.user.photoURL}
+                    src={user.photoURL}
                   />
                 </div>
               </div>
@@ -33,10 +34,9 @@ function Nav() {
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <a className="justify-between">
+                  <Link to="/profile" className="justify-between">
                     Profile
-                    <span className="badge">New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a>Settings</a>
